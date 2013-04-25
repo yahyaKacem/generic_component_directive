d28dce9504b365223e1b4926063e0e368eb5b599
yahyaKacem <fuj.tyoli@gmail.com>
2013-04-25 21:47:49 +0100

    almost done here

--- src/scripts/directives/component.js
+++ src/scripts/directives/component.js
@@ -2,23 +2,17 @@
 //######################START component###################################
 directives.component = function($compile, Logger){
   return {
-    scope: {
-      type:    "@",
-      element: "="
-    },
-    compile:function(tElement, tAttrs){
+    restrict: 'E',
+    link: function (iScope, iElement, iAttrs) {
       var log = Logger.log;
-      var el = tElement[0];
-      if(el.getAttribute('type')){
-        log(tAttrs.type);
-        el.removeAttribute('type');
-        el.setAttribute(tAttrs.type,'');
-        return function(scope, element, attrs){
-          $compile(el)(scope);
-        };
+      if(iElement[0].getAttribute('type')){
+        log(iAttrs.type);
+        iElement.attr(iAttrs.type, '');
+        iElement[0].removeAttribute('type');
+        $compile(iElement)(iScope);
       }
     }
-  }
+  };
 };
 //######################END component#####################################
 //########################################################################
